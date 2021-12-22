daa830bbcadc1370d4268a3f22c6f8807bacf102
Ammar Mahmood <ammarmahmood288@gmail.com>
2021-12-22 17:34:47 -0500

    remove part 1 from code

--- day3-p2-main.py
+++ day3-p2-main.py
@@ -1,32 +1,9 @@
 with open("sample.txt") as f:
     data = [x for x in f.read().split()]
-gamma = ""
-epsilon = ""
-
-for b in range(0, len(data[0])):
-    one = 0
-    zero = 0
-    for c in range(0, len(data)):
-        if data[c][b] == "0":
-          zero += 1
-        else:
-          one += 1
-    if zero > one:
-      gamma += "0"
-      epsilon += "0"
-    else:
-      gamma += "0"
-      epsilon += "0"
-      
-g = int (gamma, 2)
-e = int(epsilon, 2)
-print("PART 1:", g * e)
-
 
 gamma = ""
 epsilon = ""
 
-data2 = data.copy()
 index = 0
 while len(data) > 1:
     one = 0
